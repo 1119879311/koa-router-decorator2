c9b64153a5dd874494477a72ec4ca2f4eb7a08ee
1119879311 <1119879311@qq.com>
2023-03-12 20:24:27 +0800

    初始化项目

--- test/test1/Controller.ts
+++ test/test1/Controller.ts
@@ -1,91 +1,4 @@
-# koa-router-decorator2
-一个高效轻量级的基于koa 和koa-router 写的装饰器路由库
-
-#### 示例1
-> src/index.ts
-
-```typescript
-import {ResigerRouter} from "koa-router-decorator2";
-import koaRouter from "koa-router";
-import Koa from "koa"
-import { setGolbalGuards, setGolbalMiddleware, UserController, Validation } from "./UserController";
-
-const app = new Koa()
-const rotuer = new koaRouter({
-  prefix: "/api",
-});
-
-
-/**一个http服务请求的执行顺序
- *全局中间件--setGolbalMiddleware
-  类中间件--ClassMiddleware
-  方法中间件--MethondMiddleware
-  全局守卫 useAuthGuards { useConfig: { userId: '1', userName: '123' } } 12
-  类守卫 ClassGuards { useConfig: { userId: '1', userName: '123' } }
-  方法守卫 MethondGuard { useConfig: { userId: '1', userName: '123' } } 这是元数据
-  管道 Validation-apply: [ { value: { id: '12' }, type: [class useDto] } ]
-  路由层:getbefore state: { useConfig: { userId: '1', userName: '123' } } query: { id: '12' }
- * 
- */
-
-
- // 注册控制器
-  ResigerRouter(rotuer, UserController,{
-    midwares:[setGolbalMiddleware], // 中间件
-    guards:[setGolbalGuards], // 守卫
-    pipes:[ new Validation() ] // 管道: 一般用与请求的参数默认设置，转换，验证
-  });
-
-  // 全局错误捕获处理
-  const HttpExceptionFilter =  async (error:Error,ctx:Koa.DefaultContext)=>{
-    ctx.body =  {message:error.message?error.message:error,status:false,success:false,path: ctx.url,}
-  }
-
-  //全局响应处理(可以省略)
-  const HttpResponseInterceptor = async (ctx:Koa.DefaultContext)=>{
-    if (ctx.response.is("text/plain") || ctx.response.is("json")) {
-
-      ctx.body = await { timestamp: new Date(), code: 200, data: ctx.body, status: true, message: "success"};
-    }
-  }
-
-  // 设置第一个中间，可以捕获全局响应和错误处理
-  const firstMiddlWare = async (ctx:Koa.DefaultContext,next:Function)=>{
-    try {
-      await next();
-      HttpResponseInterceptor(ctx) ;//响应处理
-    } catch (error) {
-      ctx.app.emit("error", error, ctx); // 错误处理
-    }
-  }
-
-  app.use(firstMiddlWare)
-  app.use(rotuer.routes()); // 使用路由中间
-  app.listen( 8080,()=>{
-    console.log("server is run in prot 8080")
-  } )
-  app.on("error", HttpExceptionFilter)
-
-
-
-```
-
-> src/UserController.ts
-
-```typescript
-// import {
-//     Controller,
-//     GET,
-//     Query,
-//     Ctx,
-//     Guard,
-//     Pipe,
-//     IContextOption,
-//     IGuard,
-//     Use,
-//     setMetadata,
-//   } from "../../index";
-  import {
+import {
     Controller,
     GET,
     Query,
@@ -96,14 +9,14 @@ const rotuer = new koaRouter({
     IGuard,
     Use,
     setMetadata,
-  } from "koa-router-decorator2";
+  } from "../../index";
   import Koa from "koa"
 
   // DTO
   class useDto{
     id:string="12"
     name:string="name"
-
+  
   }
   // 中间件,设置全局 参数
   // const 
@@ -116,22 +29,23 @@ const rotuer = new koaRouter({
     // throw new Error("全局中间件--setGolbalMiddleware 错误")
     await next()
   }
-
+  
   //类中间件--ClassMiddleware
   export const ClassMiddleware = async(ctx:Koa.DefaultContext,next:Function)=>{
     console.log("类中间件--ClassMiddleware")
     // throw new Error("类中间件--ClassMiddleware 错误")
-
+   
     await next()
   }
-
+  
   //方法中间件--MethondMiddleware
   export const MethondMiddleware = async(ctx:Koa.DefaultContext,next:Function)=>{
     console.log("方法中间件--MethondMiddleware")
     // throw new Error("方法中间件--MethondMiddleware 错误")
     await next()
   }
-
+  
+  
   // 全局守卫
   export const setGolbalGuards:IGuard = async (option)=>{
     const {ctx } = option
@@ -140,14 +54,15 @@ const rotuer = new koaRouter({
     return true
     // return await ctx.query.id===ctx.state.useConfig.userId
   }
-
+  
+  
   // 类的守卫
-
+  
   export const ClassGuard:IGuard = async (option)=>{
     const {ctx } = option
     console.log("类守卫 ClassGuards" ,ctx.state)
     // throw new Error("类守卫 ClassGuards 错误")
-
+   
     return true
   }
   // 方法的守卫
@@ -155,30 +70,30 @@ const rotuer = new koaRouter({
     const {ctx,get } = option
     console.log("方法守卫 MethondGuard" ,ctx.state ,get("apiName"))
     // throw new Error("方法守卫 MethondGuard 错误")
-
+   
     return true
   }
-
+  
   // 管道:作用参数的转换，设置默认值，校验
   export class  Validation extends Pipe {
     apply(option: IContextOption): void | Promise<void> {
        console.log("管道 Validation-apply:", option.getArgs())
       //  throw new Error("管道 Validation-apply: 参数有误")
     }
-
+    
   }
-
+  
   @Use(ClassMiddleware)
   @Guard(ClassGuard)
   @Controller("/user")
   export  class UserController  {
-
+   
     @setMetadata("apiName",'这是元数据')
     @Use(MethondMiddleware)
     @Guard(MethondGuard)
     @GET("list")
     list(@Query() query: useDto, @Ctx() ctx: any,@Query("id") id: string,) {
-
+   
       console.log("路由层:getbefore","state:",ctx.state,"query:",query);
       return this.getd()
     }
@@ -186,8 +101,7 @@ const rotuer = new koaRouter({
       return "lallalla";
     }
   }
+  
+  
 
-```
-
-API 参数说明
-
+  
